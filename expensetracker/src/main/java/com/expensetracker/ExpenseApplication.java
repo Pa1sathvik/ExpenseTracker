ceb11877aa34adc5c907721b4028df61675276e4
svutukur <svutukur@INWPF1VRP2C-AAD>
2020-08-13 07:24:37 +0530

    Correct doc

--- expensetracker/src/main/java/com/expensetracker/ExpenseApplication.java
+++ expensetracker/src/main/java/com/expensetracker/ExpenseApplication.java
@@ -5,16 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 /**
- * Hello world!
+ * 
+ * Main Spring boot application.
+ * @author svutukur
  *
  */
-
 @EnableCaching
 @SpringBootApplication
 public class ExpenseApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(ExpenseApplication.class, args);
-		
+
 	}
 }
--- expensetracker/src/main/java/com/expensetracker/model/Category.java
+++ expensetracker/src/main/java/com/expensetracker/model/Category.java
@@ -1,14 +1,11 @@
 package com.expensetracker.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 @Entity
 @NoArgsConstructor
