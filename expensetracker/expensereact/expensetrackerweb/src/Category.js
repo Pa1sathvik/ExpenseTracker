37fb690348c2b25100f3adf09d44915babccaf6e
Vutukuri <svutukuri@informatica.com>
2020-08-14 08:59:42 +0530

    First commit

---
+++ expensetracker/expensereact/expensetrackerweb/src/Category.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Category extends Component {
+
+    constructor(props){
+
+        this.props = props;
+        state = {
+
+            isLoading : true,
+            categories:[],
+        }
+    }
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Category;
